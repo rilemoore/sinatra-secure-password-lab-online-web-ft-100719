f92567b6c6a34c9afac237e314f46bd1685d8d56
Riley Moore <rilemoore@gmail.com>
2019-11-30 22:40:03 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,7 +17,7 @@ class ApplicationController < Sinatra::Base
   end
 
   post "/signup" do
-    if !params[:username]
+    if params[:username].empty?
       redirect '/failure'
     end
     user = User.new(:username => params[:username], :password => params[:password])
